ba4b315ba749925040a1cb6688ac59460aea7681
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-08-27 20:27:51 -0500

    permission management

--- app/src/main/java/com/example/apphoroscopo/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/apphoroscopo/ui/palmistry/PalmistryFragment.kt
@@ -1,10 +1,14 @@
 package com.example.apphoroscopo.ui.palmistry
 
+import android.Manifest.permission.CAMERA
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import com.example.apphoroscopo.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +16,31 @@ import dagger.hilt.android.AndroidEntryPoint
 class PalmistryFragment : Fragment() {
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted ->
+        if (isGranted){
+            //start
+        }else{
+            Toast.makeText(requireContext(), "Acepta los permissos para poder disfrutar de una experiencia mágica", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()){
+            //start
+        }else{
+            requestPermissionLauncher.launch(CAMERA)
+        }
+    }
+
+    fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(requireContext(), CAMERA) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
